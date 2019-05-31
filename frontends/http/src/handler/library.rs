a63c03eb37cf1e91b7d833078d14c8241a54644e
Max Jöhnk <maxjoehnk@gmail.com>
2019-05-31 12:00:48 +0200

    add stopwatch to single album fetching

--- frontends/http/src/handler/library.rs
+++ frontends/http/src/handler/library.rs
@@ -5,34 +5,43 @@ use viewmodels::*;
 
 pub fn fetch_album(album_id: usize, rustic: &Arc<Rustic>) -> Result<Option<AlbumModel>, Error> {
     let library = &rustic.library;
+
+    let sw = stopwatch::Stopwatch::start_new();
     let mut query = SingleQuery::id(album_id);
     query.join_all();
-    let album = library.query_album(query)?.map(|album| AlbumModel::new(album, &rustic));
+    let album = library.query_album(query)?
+        .map(|album| AlbumModel::new(album, &rustic));
+    debug!("Fetching album took {}ms", sw.elapsed_ms());
 
     Ok(album)
 }
 
 pub fn fetch_albums(rustic: &Arc<Rustic>) -> Result<Vec<AlbumModel>, Error> {
     let library = &rustic.library;
+
     let sw = stopwatch::Stopwatch::start_new();
     let mut query = MultiQuery::new();
     query.join_artists();
     let albums = library
         .query_albums(query)?;
     debug!("Fetching albums took {}ms", sw.elapsed_ms());
+
     let albums = albums
         .into_iter()
         .map(|album| AlbumModel::new(album, &rustic))
         .collect();
+
     Ok(albums)
 }
 
 pub fn fetch_artists(rustic: &Arc<Rustic>) -> Result<Vec<ArtistModel>, Error> {
     let library = &rustic.library;
+
     let sw = stopwatch::Stopwatch::start_new();
     let artists = library
         .query_artists(MultiQuery::new())?;
     debug!("Fetching artists took {}ms", sw.elapsed_ms());
+
     let artists = artists
         .into_iter()
         .map(|artist| ArtistModel::new(artist, &rustic))
