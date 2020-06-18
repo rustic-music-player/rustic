afc00273dea5a623f62a4cbfe9de73637ba98735
Max Jöhnk <maxjoehnk@gmail.com>
2020-06-18 07:01:06 +0200

    fix(store/sqlite): add missing provider mapping

--- store/sqlite/src/entities/provider.rs
+++ store/sqlite/src/entities/provider.rs
@@ -28,16 +28,8 @@ where
     i32: FromSql<Integer, DB>,
 {
     fn from_sql(bytes: Option<&<DB as Backend>::RawValue>) -> deserialize::Result<Self> {
-        match i32::from_sql(bytes)? {
-            0 => Ok(ProviderType::Pocketcasts),
-            1 => Ok(ProviderType::Soundcloud),
-            2 => Ok(ProviderType::GooglePlayMusic),
-            3 => Ok(ProviderType::Spotify),
-            4 => Ok(ProviderType::LocalMedia),
-            5 => Ok(ProviderType::Youtube),
-            _ => Err(format!("someone tampered with the data").into()),
-        }
-        .map(SerializedProvider::from)
+        let provider = int_to_provider(i32::from_sql(bytes)?);
+        Ok(SerializedProvider(provider))
     }
 }
 
@@ -47,37 +39,32 @@ where
     i32: ToSql<Integer, DB>,
 {
     fn to_sql<W: Write>(&self, out: &mut Output<W, DB>) -> serialize::Result {
-        let p = match self.0 {
-            ProviderType::Pocketcasts => 0,
-            ProviderType::Soundcloud => 1,
-            ProviderType::GooglePlayMusic => 2,
-            ProviderType::Spotify => 3,
-            ProviderType::LocalMedia => 4,
-            ProviderType::Youtube => 5,
-        };
+        let p = provider_to_int(self.0);
         p.to_sql(out)
     }
 }
 
 pub fn provider_to_int(provider: ProviderType) -> i32 {
     match provider {
-        ProviderType::Pocketcasts => 0,
-        ProviderType::Soundcloud => 1,
-        ProviderType::GooglePlayMusic => 2,
-        ProviderType::Spotify => 3,
-        ProviderType::LocalMedia => 4,
-        ProviderType::Youtube => 5,
+        ProviderType::Internal => 0,
+        ProviderType::Pocketcasts => 1,
+        ProviderType::Soundcloud => 2,
+        ProviderType::GooglePlayMusic => 3,
+        ProviderType::Spotify => 4,
+        ProviderType::LocalMedia => 5,
+        ProviderType::Youtube => 6,
     }
 }
 
 pub fn int_to_provider(provider: i32) -> ProviderType {
     match provider {
-        0 => ProviderType::Pocketcasts,
-        1 => ProviderType::Soundcloud,
-        2 => ProviderType::GooglePlayMusic,
-        3 => ProviderType::Spotify,
-        4 => ProviderType::LocalMedia,
-        5 => ProviderType::Youtube,
+        0 => ProviderType::Internal,
+        1 => ProviderType::Pocketcasts,
+        2 => ProviderType::Soundcloud,
+        3 => ProviderType::GooglePlayMusic,
+        4 => ProviderType::Spotify,
+        5 => ProviderType::LocalMedia,
+        6 => ProviderType::Youtube,
         _ => unreachable!("someone tampered with the data"),
     }
 }
