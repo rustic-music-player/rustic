4f4d832fe3f095b2c38e67123aac8cc0da55dd2f
Max Jöhnk <maxjoehnk@gmail.com>
2021-01-03 13:54:14 +0100

    feat(systray): remove windows implementation of systray frontend
    systray dependency is discontinued, will replace with custom implementation

--- frontends/systray/src/lib.rs
+++ frontends/systray/src/lib.rs
@@ -1,16 +1,5 @@
 use failure::Error;
 
-#[cfg(target_os = "windows")]
-pub fn start() -> Result<(), Error> {
-    let mut app = systray::Application::new()?;
-    let icon = include_bytes!("../../../assets/logo.png");
-    app.set_icon_from_buffer(icon, 1000, 1000)?;
-    app.set_tooltip("Rustic")?;
-
-    Ok(())
-}
-
-#[cfg(not(target_os = "windows"))]
 pub fn start() -> Result<(), Error> {
     Ok(())
 }
