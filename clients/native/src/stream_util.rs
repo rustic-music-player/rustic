bbed99e1a5ac5e55fb86378f1f40723d240c1ad4
Max Jöhnk <maxjoehnk@gmail.com>
2021-01-06 16:30:41 +0100

    fix(clients/native): don't pin cpu because of channel polling

--- clients/native/src/stream_util.rs
+++ clients/native/src/stream_util.rs
@@ -1,27 +1,48 @@
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-use crossbeam_channel::{Receiver, TryRecvError};
+use crossbeam_channel::{Receiver, Sender, TryRecvError};
 use futures::Stream;
+use std::thread;
+use std::fmt::Debug;
 
 pub fn from_channel<T>(recv: Receiver<T>) -> StreamingReceiver<T> {
-    StreamingReceiver(recv)
+    let (inner_tx, inner_rx) = crossbeam_channel::unbounded();
+
+    StreamingReceiver {
+        inner_rx,
+        inner_tx,
+        outer_rx: recv,
+    }
 }
 
-pub struct StreamingReceiver<T>(Receiver<T>);
+pub struct StreamingReceiver<T> {
+    inner_rx: Receiver<T>,
+    inner_tx: Sender<T>,
+    outer_rx: Receiver<T>,
+}
 
-impl<T> Stream for StreamingReceiver<T> {
+impl<T: Send + 'static + Debug> Stream for StreamingReceiver<T> {
     type Item = T;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let result = self.0.try_recv();
+        let receiver = self.outer_rx.clone();
+        let sender = self.inner_tx.clone();
+        let waker = cx.waker().clone();
+        // TODO: use thread pool or tokio runtime? replace crossbeam channels with async native channels?
+        thread::spawn(move || {
+            let result = receiver.recv();
+            log::trace!("streaming receiver waker: {:?}", result);
+            if let Ok(value) = result {
+                sender.send(value);
+                waker.wake();
+            }
+        });
+        let result = self.inner_rx.try_recv();
+        log::trace!("streaming receiver: {:?}", result);
         match result {
             Ok(item) => Poll::Ready(Some(item)),
-            Err(TryRecvError::Empty) => {
-                // TODO: we should keep track of the wakers and only wake when a new event is emitted
-                cx.waker().wake_by_ref();
-                Poll::Pending
-            }
+            Err(TryRecvError::Empty) => Poll::Pending,
             Err(TryRecvError::Disconnected) => Poll::Ready(None),
         }
     }
