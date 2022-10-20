d7ce0b5265073504377357d9bc61eca57b38f3f6
Max Jöhnk <maxjoehnk@gmail.com>
2022-10-20 17:05:26 +0200

    feat(frontends/dbus): proper implement dbus frontend and promote to default feature

--- frontends/dbus/src/lib.rs
+++ frontends/dbus/src/lib.rs
@@ -1,154 +1,219 @@
 use std::collections::HashMap;
-use std::sync::Arc;
 
-use dbus::channel::MatchingReceiver;
-use dbus::message::MatchRule;
-use dbus_crossroads::Crossroads;
-use dbus_tokio::connection;
-use failure::{format_err, Error};
-use log::{debug, error};
+use failure::{Error};
+use zbus::{ConnectionBuilder, dbus_interface};
+use zbus::export::futures_util::StreamExt;
+use zbus::zvariant::{Value};
 
-use rustic_api::models::TrackModel;
+use rustic_api::models::PlayerEventModel;
 use rustic_api::ApiClient;
 
-struct PlayerState {
-    playing: bool,
-    track: Option<TrackModel>,
-    volume: f32,
-    client: ApiClient,
-}
+const MPRIS_PATH: &str = "/org/mpris/MediaPlayer2";
 
 pub async fn start(client: ApiClient) -> Result<(), Error> {
-    let (resource, conn) = connection::new_session_sync()?;
+    let player = MprisPlayer { client: client.clone() };
 
-    tokio::spawn(async {
-        let err = resource.await;
-        error!("Lost connection to D-Bus: {}", err);
-    });
-
-    conn.request_name("org.mpris.MediaPlayer2.rustic", false, true, false)
+    let connection = ConnectionBuilder::session()?
+        .name("org.mpris.MediaPlayer2.rustic")?
+        .serve_at(MPRIS_PATH, MprisPlayerIdentity)?
+        .serve_at(MPRIS_PATH, player)?
+        .build()
         .await?;
 
-    let mut cr = Crossroads::new();
-    cr.set_async_support(Some((
-        conn.clone(),
-        Box::new(|x| {
-            tokio::spawn(x);
-        }),
-    )));
+    tokio::task::spawn(async move {
+        let player_ref = connection
+            .object_server()
+            .interface::<_, MprisPlayer>(MPRIS_PATH)
+            .await
+            .unwrap();
 
-    cr.insert("/", &[cr.introspectable(), cr.properties()], ());
+        while let Some(event) = client.observe_player(None).next().await {
+            let player = player_ref.get_mut().await;
 
-    add_player(&mut cr, &client).await?;
-
-    conn.start_receive(
-        MatchRule::new_method_call(),
-        Box::new(move |msg, conn| {
-            debug!("Received msg {:?}", &msg);
-            let msg_string = format!("{:?}", &msg);
-            if cr.handle_message(msg, conn).is_err() {
-                error!("Could not handle message {}", msg_string);
+            match event {
+                PlayerEventModel::VolumeChanged(_) => player.volume_changed(player_ref.signal_context()).await.unwrap(),
+                PlayerEventModel::StateChanged(_) => player.playback_status_changed(player_ref.signal_context()).await.unwrap(),
+                PlayerEventModel::TrackChanged(_) => player.metadata_changed(player_ref.signal_context()).await.unwrap(),
+                _ => {}
             }
-            true
-        }),
-    );
+        }
+    });
 
     Ok(())
 }
 
-async fn add_player(cr: &mut Crossroads, client: &ApiClient) -> Result<(), Error> {
-    let player = client
-        .get_player(None)
-        .await?
-        .ok_or_else(|| format_err!("missing player"))?;
-    let player_state = PlayerState {
-        client: Arc::clone(client),
-        playing: player.playing,
-        track: player.current,
-        volume: player.volume,
-    };
-    let identity_token = cr.register::<PlayerState, _, _>("org.mpris.MediaPlayer2", |b| {
-        b.property("Identity")
-            .get(|_, _| Ok(String::from("Rustic Music Player")));
-        b.property("CanQuit").get(|_, _| Ok(false));
-        b.property("CanRaise").get(|_, _| Ok(false));
-        b.property("HasTrackList").get(|_, _| Ok(false));
-        b.property("SupportedUriSchemes")
-            .get(|_, _| Ok(vec!["http".to_string(), "file".to_string()]));
-        b.property("SupportedMimeTypes")
-            .get(|_, _| Ok(vec!["audio/mpeg".to_string(), "audio/wav".to_string()]));
-    });
+struct MprisPlayerIdentity;
 
-    let player_token = cr.register::<PlayerState, _, _>("org.mpris.MediaPlayer2.Player", |b| {
-        b.property("PlaybackStatus").get(|_, state| {
-            if state.playing {
-                Ok(String::from("Playing"))
-            } else if state.track.is_some() {
-                Ok(String::from("Paused"))
-            } else {
-                Ok(String::from("Stopped"))
-            }
-        });
-        b.property("Volume").get(|_, state| {
-            let volume = state.volume as f64;
-            Ok(volume)
-        });
-        b.property("CanSeek").get(|_, _| Ok(false));
-        b.property("CanGoNext").get(|_, _| Ok(false));
-        b.property("CanGoPrevious").get(|_, _| Ok(false));
-        b.property("CanPlay").get(|_, _| Ok(true));
-        b.property("CanPause").get(|_, _| Ok(false));
-        b.property("Metadata").get(|_, state| {
-            if let Some(track) = state.track.as_ref() {
-                let mut metadata = HashMap::new();
-                if let Some(ref album) = track.album {
-                    metadata.insert("xesam:album".to_string(), album.title.clone());
-                }
-                metadata.insert("xesam:title".to_string(), track.title.clone());
-                Ok(metadata)
-            } else {
-                Ok(HashMap::new())
-            }
-        });
-        b.method_with_cr_async("Play", (), (), |mut ctx, cr, _: ()| {
-            let client: ApiClient =
-                Arc::clone(&cr.data_mut::<PlayerState>(ctx.path()).unwrap().client);
-            async move {
-                client.player_control_play(None).await.unwrap();
-                ctx.reply(Ok(()))
-            }
-        });
-        b.method_with_cr_async("Pause", (), (), |mut ctx, cr, _: ()| {
-            let client: ApiClient =
-                Arc::clone(&cr.data_mut::<PlayerState>(ctx.path()).unwrap().client);
-            async move {
-                client.player_control_pause(None).await.unwrap();
-                ctx.reply(Ok(()))
-            }
-        });
-        b.method_with_cr_async("Next", (), (), |mut ctx, cr, _: ()| {
-            let client: ApiClient =
-                Arc::clone(&cr.data_mut::<PlayerState>(ctx.path()).unwrap().client);
-            async move {
-                client.player_control_next(None).await.unwrap();
-                ctx.reply(Ok(()))
-            }
-        });
-        b.method_with_cr_async("Previous", (), (), |mut ctx, cr, _: ()| {
-            let client: ApiClient =
-                Arc::clone(&cr.data_mut::<PlayerState>(ctx.path()).unwrap().client);
-            async move {
-                client.player_control_prev(None).await.unwrap();
-                ctx.reply(Ok(()))
-            }
-        });
-    });
+#[dbus_interface(name = "org.mpris.MediaPlayer2")]
+impl MprisPlayerIdentity {
+    #[dbus_interface(property)]
+    fn identity(&self) -> String {
+        "Rustic Music Player".to_string()
+    }
 
-    cr.insert(
-        "/org/mpris/MediaPlayer2",
-        &[identity_token, player_token],
-        player_state,
-    );
+    #[dbus_interface(property)]
+    fn can_quit(&self) -> bool {
+        false
+    }
 
-    Ok(())
+    #[dbus_interface(property)]
+    fn can_raise(&self) -> bool {
+        false
+    }
+
+    #[dbus_interface(property)]
+    fn has_track_list(&self) -> bool {
+        false
+    }
+
+    #[dbus_interface(property)]
+    fn supported_uri_schemes(&self) -> Vec<String> {
+        vec![
+            "http".to_string(),
+            "file".to_string()
+        ]
+    }
+
+    #[dbus_interface(property)]
+    fn supported_mime_types(&self) -> Vec<String> {
+        vec![
+            "audio/mpeg".to_string(),
+            "audio/wav".to_string()
+        ]
+    }
+}
+
+struct MprisPlayer {
+    client: ApiClient
+}
+
+#[dbus_interface(name = "org.mpris.MediaPlayer2.Player")]
+impl MprisPlayer {
+    #[dbus_interface(property)]
+    fn can_seek(&self) -> bool {
+        false
+    }
+
+    #[dbus_interface(property)]
+    fn can_go_next(&self) -> bool {
+        true
+    }
+
+    #[dbus_interface(property)]
+    fn can_go_previous(&self) -> bool {
+        true
+    }
+
+    #[dbus_interface(property)]
+    fn can_control(&self) -> bool {
+        true
+    }
+
+    #[dbus_interface(property)]
+    fn can_play(&self) -> bool {
+        true
+    }
+
+    #[dbus_interface(property)]
+    fn can_pause(&self) -> bool {
+        true
+    }
+
+    #[dbus_interface(property)]
+    async fn playback_status(&self) -> String {
+        let player = self.client.get_player(None).await.unwrap().unwrap();
+
+        if player.playing {
+            "Playing"
+        } else if player.current.is_some() {
+            "Paused"
+        } else {
+            "Stopped"
+        }.to_string()
+    }
+
+    #[dbus_interface(property)]
+    async fn metadata(&self) -> HashMap<&str, Value> {
+        let player = self.client.get_player(None).await.unwrap().unwrap();
+
+        let mut metadata = HashMap::new();
+
+        if let Some(track) = player.current {
+            metadata.insert("mpris:trackid", track.cursor.into());
+            metadata.insert("xesam:title", track.title.into());
+            if let Some(duration) = track.duration {
+                metadata.insert("mpris:length", duration.into());
+            }
+            if let Some(artist) = track.artist {
+                metadata.insert("xesam:artist", artist.name.into());
+            }
+            if let Some(album) = track.album {
+                metadata.insert("xesam:album", album.title.into());
+            }
+            if let Some(coverart) = track.coverart {
+                metadata.insert("mpris:artUrl", format!("http://127.0.0.1:8080{coverart}").into()); // TODO: get http frontend base url
+            }
+        }
+
+        metadata
+    }
+
+    #[dbus_interface(property)]
+    async fn volume(&self) -> f32 {
+        let player = self.client.get_player(None).await.unwrap().unwrap();
+
+        player.volume
+    }
+
+    #[dbus_interface(property)]
+    async fn set_volume(&self, volume: f64) -> zbus::Result<()> {
+        self.client.player_set_volume(None, volume as f32).await.map_err(to_zbus_error)
+    }
+
+    async fn play(&self) -> zbus::fdo::Result<()> {
+        self.client.player_control_play(None).await.map_err(to_zbus_fdo_error)
+    }
+
+    async fn pause(&self) -> zbus::fdo::Result<()> {
+        self.client.player_control_pause(None).await.map_err(to_zbus_fdo_error)
+    }
+
+    async fn stop(&self) -> zbus::fdo::Result<()> {
+        self.client.player_control_pause(None).await.map_err(to_zbus_fdo_error)?;
+        self.client.clear_queue(None).await.map_err(to_zbus_fdo_error)?;
+
+        Ok(())
+    }
+
+    async fn play_pause(&self) -> zbus::fdo::Result<()> {
+        let player = self.client.get_player(None).await.map_err(to_zbus_fdo_error)?.unwrap();
+
+        if player.playing {
+            self.client.player_control_pause(None).await.map_err(to_zbus_fdo_error)?;
+        }else {
+            self.client.player_control_play(None).await.map_err(to_zbus_fdo_error)?;
+        }
+
+        Ok(())
+    }
+
+    async fn next(&self) -> zbus::fdo::Result<()> {
+        self.client.player_control_next(None).await.map_err(to_zbus_fdo_error)?;
+
+        Ok(())
+    }
+
+    async fn previous(&self) -> zbus::fdo::Result<()> {
+        self.client.player_control_prev(None).await.map_err(to_zbus_fdo_error)?;
+
+        Ok(())
+    }
+}
+
+fn to_zbus_fdo_error(err: Error) -> zbus::fdo::Error {
+    zbus::fdo::Error::Failed(err.to_string())
+}
+
+fn to_zbus_error(err: Error) -> zbus::Error {
+    zbus::Error::FDO(Box::new(to_zbus_fdo_error(err)))
 }
